9543a67df87fc297ec9c760f0042182b254bc4f3
Andrew Palardy <apalrd@gmail.com>
2026-05-07 19:28:09 +0300

    start work on geodb

--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.24.4
 
 require (
 	github.com/miekg/dns v1.1.72 // indirect
+	github.com/oschwald/maxminddb-golang/v2 v2.2.0 // indirect
 	golang.org/x/mod v0.31.0 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
-	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/tools v0.40.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,109 +1,145 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "net"
-    "log"
-    "flag"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"net/netip"
+	"os"
 	"strconv"
-    "strings"
-    "time"
-    "math/rand"
-    "gopkg.in/yaml.v3"
-    "github.com/miekg/dns"
+	"strings"
+	"time"
+
+	"github.com/miekg/dns"
+	"github.com/oschwald/maxminddb-golang/v2"
+	"gopkg.in/yaml.v3"
 )
 
-//configuration structure
+// configuration structure
 type Config struct {
-    Listen    	string   `yaml:"listen"`     // clients connect to us e.g. "[::]:53"
-    GeoDb       string   `yaml:"geodb"`      // path to geo-ip database (.mmdb)
-    Domain      string   `yaml:"domain"`     // domain name we respond to (we REFUSE everything else)
-    PoolStr     string   `yaml:"pool"`       // address pool for serving http data (cidr-notation)
-    PoolIP      *net.IPNet                   // decoded from pool
-    Nameserver  string   `yaml:"ns"`         // nameserver name (fqdn)
-    Rname        string   `yaml:"rname"`     // rname/mailbox (for soa record)
+	Listen     string     `yaml:"listen"` // clients connect to us e.g. "[::]:53"
+	GeoDb      string     `yaml:"geodb"`  // path to geo-ip database (.mmdb)
+	Domain     string     `yaml:"domain"` // domain name we respond to (we REFUSE everything else)
+	PoolStr    string     `yaml:"pool"`   // address pool for serving http data (cidr-notation)
+	PoolIP     *net.IPNet // decoded from pool
+	Nameserver string     `yaml:"ns"`    // nameserver name (fqdn)
+	Rname      string     `yaml:"rname"` // rname/mailbox (for soa record)
 }
 
 var cfg Config
 
-//load config
-func loadConfig(path string) (error) {
-    f, err := os.Open(path)
-    if err != nil {
-        return err
-    }
-    defer f.Close()
+// load config
+func loadConfig(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
-    if err := yaml.NewDecoder(f).Decode(&cfg); err != nil {
-        return err
-    }
+	if err := yaml.NewDecoder(f).Decode(&cfg); err != nil {
+		return err
+	}
 
-    //validate ip net
-    ip, network, err := net.ParseCIDR(cfg.PoolStr)
-    if err != nil {
-        return fmt.Errorf("invalid CIDR: %w", err)
-    }
-    if ip.Equal(network.IP) == false {
-        return fmt.Errorf("CIDR must be a network address, not a host address")
-    }
-    mask, total := network.Mask.Size()
-    if (total - mask) < 32 {
-        return fmt.Errorf("CIDR must have at least 32 bits to work with (mask %d total %d)",mask,total)
-    }
-    cfg.PoolIP = network
+	//validate ip net
+	ip, network, err := net.ParseCIDR(cfg.PoolStr)
+	if err != nil {
+		return fmt.Errorf("invalid CIDR: %w", err)
+	}
+	if ip.Equal(network.IP) == false {
+		return fmt.Errorf("CIDR must be a network address, not a host address")
+	}
+	mask, total := network.Mask.Size()
+	if (total - mask) < 32 {
+		return fmt.Errorf("CIDR must have at least 32 bits to work with (mask %d total %d)", mask, total)
+	}
+	cfg.PoolIP = network
 
-    //validate listen
-    if cfg.Listen == "" {
-        log.Printf("Listen not specified, using default")
-        cfg.Listen = "[::]:53"
-    }
+	//validate listen
+	if cfg.Listen == "" {
+		log.Printf("Listen not specified, using default")
+		cfg.Listen = "[::]:53"
+	}
 
-    //validate domain is fully qualified
-    if !strings.HasSuffix(cfg.Domain,".") {
-        return fmt.Errorf("Domain not specified or not fully-qualified (must end with .)")
-    }
+	//validate domain is fully qualified
+	if !strings.HasSuffix(cfg.Domain, ".") {
+		return fmt.Errorf("Domain not specified or not fully-qualified (must end with .)")
+	}
 
-    //validate nameserver is fully qualified
-    if !strings.HasSuffix(cfg.Nameserver,".") {
-        return fmt.Errorf("Nameserver not specified or not fully-qualified (must end with .)")
-    }
+	//validate nameserver is fully qualified
+	if !strings.HasSuffix(cfg.Nameserver, ".") {
+		return fmt.Errorf("Nameserver not specified or not fully-qualified (must end with .)")
+	}
 
-    //validate rname is fully qualified
-    if !strings.HasSuffix(cfg.Rname,".") {
-        return fmt.Errorf("Rname not specified or not fully-qualified (must end with .)")
-    }
+	//validate rname is fully qualified
+	if !strings.HasSuffix(cfg.Rname, ".") {
+		return fmt.Errorf("Rname not specified or not fully-qualified (must end with .)")
+	}
 
+	return nil
+}
 
-    return nil
+type GeoRecord struct {
+	Country struct {
+		ISOCode string            `maxminddb:"iso_code"`
+		Names   map[string]string `maxminddb:"names"`
+	} `maxminddb:"country"`
+	City struct {
+		Names map[string]string `maxminddb:"names"`
+	} `maxminddb:"city"`
+}
+
+var GeoDb *maxminddb.Reader
+
+// load geodb
+func loadGeoDb() {
+	db, err := maxminddb.Open(cfg.GeoDb)
+	if err != nil {
+		log.Fatal(err)
+	}
+	GeoDb = db
+}
+
+func queryGeoDb(src netip.Addr) GeoRecord {
+	var record GeoRecord
+	err := GeoDb.Lookup(src).Decode(&record)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Country: %s (%s)\n", record.Country.Names["en"], record.Country.ISOCode)
+	fmt.Printf("City: %s\n", record.City.Names["en"])
+	return record
 }
 
 // database structure
 type DbEntry struct {
-    //data related to DNS query
-    dnsTime     time.Time
-    dnsSrc      net.Addr
-    dnsECS      net.IP
-    //data related to HTTP queries
-    queriedAt   time.Time
+	//data related to DNS query
+	dnsTime time.Time
+	dnsSrc  net.Addr
+	dnsECS  net.IP
+	dnsGeo  GeoRecord
+    dnsECSGeo GeoRecord
+	//data related to HTTP queries
+	queriedAt time.Time
 }
 
 // database itself
-var db map[uint32]DbEntry
+var QueryDb map[uint32]DbEntry
 
-//the soa for our domain
-//generated by listenAndServeDNS
+// the soa for our domain
+// generated by listenAndServeDNS
 var soa *dns.SOA
 
-
-//handle a DNS request
+// handle a DNS request
 func handleDNSRequest(w dns.ResponseWriter, req *dns.Msg) {
 
-    //convert qtype into string for logging later
+	//convert qtype into string for logging later
 	q := req.Question[0]
 	qTypeStr := strconv.Itoa(int(q.Qtype))
 	switch q.Qtype {
-	case dns.TypeA: 
+	case dns.TypeA:
 		qTypeStr = "A"
 	case dns.TypeAAAA:
 		qTypeStr = "AAAA"
@@ -117,134 +153,151 @@ func handleDNSRequest(w dns.ResponseWriter, req *dns.Msg) {
 		qTypeStr = "PTR"
 	}
 
-    // RemoteAddr() is available directly on ResponseWriter
-    remoteAddr := w.RemoteAddr()
-    
-    // Extract just the IP (strips the port)
-    clientIP, _, err := net.SplitHostPort(remoteAddr.String())
-    if err != nil {
-        // RemoteAddr had no port (unlikely but safe to handle)
-        clientIP = remoteAddr.String()
-    }
+	// RemoteAddr() is available directly on ResponseWriter
+	remoteAddr := w.RemoteAddr()
 
-    //validate that query is for our domain (DNS will randomize case)
-    if !strings.EqualFold(q.Name,cfg.Domain) {
-        log.Printf("Got query of invalid name: [%s] %s",qTypeStr,q.Name)
-        m := new(dns.Msg)
-        m.SetRcode(req, dns.RcodeRefused)
-        _ = w.WriteMsg(m)
-        return
-    }
+	// Extract just the IP (strips the port)
+	clientIP, _, err := net.SplitHostPort(remoteAddr.String())
+	if err != nil {
+		// RemoteAddr had no port (unlikely but safe to handle)
+		clientIP = remoteAddr.String()
+	}
 
-    //check if question is not an AAAA -> return NOERROR / NOANSWER
-    //need SOA record for this one
-    if q.Qtype != dns.TypeAAAA {
-        log.Printf("Got query of incorrect type: [%s] from %s",qTypeStr,clientIP)
-        m := new(dns.Msg)
-        m.SetRcode(req, dns.RcodeSuccess)
-        m.Ns = []dns.RR{soa}
-        m.Authoritative = true
-        _ = w.WriteMsg(m)
-        return
-    }
+	//conver to netIP
+	reqIP, err := netip.ParseAddr(clientIP)
+	if err != nil {
+		// invalid?
+		log.Printf("Unable to parse netIP: %s", clientIP)
+		return
+	}
 
-    id := rand.Uint32()
-    var entry DbEntry
+	//validate that query is for our domain (DNS will randomize case)
+	if !strings.EqualFold(q.Name, cfg.Domain) {
+		log.Printf("Got query of invalid name: [%s] %s", qTypeStr, q.Name)
+		m := new(dns.Msg)
+		m.SetRcode(req, dns.RcodeRefused)
+		_ = w.WriteMsg(m)
+		return
+	}
 
-    log.Printf("Got query of correct type: [%s] uid %d",qTypeStr,id)
-    entry.dnsTime = time.Now()
-    entry.dnsSrc = remoteAddr
-    
-    log.Printf("Query from IP: %s", clientIP)
+	//check if question is not an AAAA -> return NOERROR / NOANSWER
+	//need SOA record for this one
+	if q.Qtype != dns.TypeAAAA {
+		log.Printf("Got query of incorrect type: [%s] from %s", qTypeStr, clientIP)
+		m := new(dns.Msg)
+		m.SetRcode(req, dns.RcodeSuccess)
+		m.Ns = []dns.RR{soa}
+		m.Authoritative = true
+		_ = w.WriteMsg(m)
+		return
+	}
 
-    // get edns client subnet
-    if subnet := getECS(req); subnet != nil {
-        log.Printf("ECS Address:        %s", subnet.Address)
-        log.Printf("ECS Source prefix:  %d", subnet.SourceNetmask)
-        log.Printf("ECS Scope prefix:   %d", subnet.SourceScope)
-        log.Printf("ECS Family:         %d (1=IPv4, 2=IPv6)", subnet.Family)
-        entry.dnsECS = subnet.Address
-    } else {
-        log.Println("No EDNS Client Subnet in query")
-    }
+	id := rand.Uint32()
+	var entry DbEntry
 
-    //store data in db
-    db[id] = entry
+	log.Printf("Got query of correct type: [%s] uid %d", qTypeStr, id)
+	entry.dnsTime = time.Now()
+	entry.dnsSrc = remoteAddr
 
-    log.Printf("DB now has %d entries",len(db))
+	log.Printf("Query from IP: %s", clientIP)
 
-    m := new(dns.Msg)
-    m.SetRcode(req, dns.RcodeSuccess)
+	entry.dnsGeo = queryGeoDb(reqIP)
+
+	// get edns client subnet
+	if subnet := getECS(req); subnet != nil {
+		log.Printf("ECS Address:        %s", subnet.Address)
+		log.Printf("ECS Source prefix:  %d", subnet.SourceNetmask)
+		log.Printf("ECS Scope prefix:   %d", subnet.SourceScope)
+		log.Printf("ECS Family:         %d (1=IPv4, 2=IPv6)", subnet.Family)
+		entry.dnsECS = subnet.Address
+        ecs,_ := netip.ParseAddr(subnet.Address.String())
+        entry.dnsECSGeo = queryGeoDb(ecs)
+	} else {
+		log.Println("No EDNS Client Subnet in query")
+	}
+
+	//store data in db
+	QueryDb[id] = entry
+
+	log.Printf("DB now has %d entries", len(QueryDb))
+
+	m := new(dns.Msg)
+	m.SetRcode(req, dns.RcodeSuccess)
 	_ = w.WriteMsg(m)
 }
 
-//parse opt for edns client subnet
+// parse opt for edns client subnet
 func getECS(r *dns.Msg) *dns.EDNS0_SUBNET {
-    opt := r.IsEdns0()
-    if opt == nil {
-        return nil // No OPT record at all
-    }
+	opt := r.IsEdns0()
+	if opt == nil {
+		return nil // No OPT record at all
+	}
 
-    for _, option := range opt.Option {
-        if subnet, ok := option.(*dns.EDNS0_SUBNET); ok {
-            return subnet
-        }
-    }
-    return nil
+	for _, option := range opt.Option {
+		if subnet, ok := option.(*dns.EDNS0_SUBNET); ok {
+			return subnet
+		}
+	}
+	return nil
 }
 
-//statically configure an SOA record
+// statically configure an SOA record
 func genSOA() {
-    //gen timestamp for SOA record
-    curSerial,_ := strconv.Atoi(time.Now().Format("2006010215"))
-    soa = &dns.SOA{
-        Hdr:     dns.RR_Header{Name: cfg.Domain, Rrtype: dns.TypeSOA, Class: dns.ClassINET, Ttl: 300},
-        Ns:      cfg.Nameserver,
-        Mbox:    cfg.Rname,
-        Serial:  uint32(curSerial),
-        Refresh: 3600,
-        Retry:   900,
-        Expire:  604800,
-        Minttl:  300, // negative caching TTL per RFC 2308
-    }
+	//gen timestamp for SOA record
+	curSerial, _ := strconv.Atoi(time.Now().Format("2006010215"))
+	soa = &dns.SOA{
+		Hdr:     dns.RR_Header{Name: cfg.Domain, Rrtype: dns.TypeSOA, Class: dns.ClassINET, Ttl: 300},
+		Ns:      cfg.Nameserver,
+		Mbox:    cfg.Rname,
+		Serial:  uint32(curSerial),
+		Refresh: 3600,
+		Retry:   900,
+		Expire:  604800,
+		Minttl:  300, // negative caching TTL per RFC 2308
+	}
 }
 
-//listen and serve DNS
+// listen and serve DNS
 func listenAndServeDNS() error {
-    //gen new SOA record
-    genSOA()
+	//gen new SOA record
+	genSOA()
 
-    //create new dns server
-    mux := dns.NewServeMux()
-    mux.HandleFunc(".", handleDNSRequest)
+	//create new dns server
+	mux := dns.NewServeMux()
+	mux.HandleFunc(".", handleDNSRequest)
 
-    server := &dns.Server{
-        Addr:    cfg.Listen,
-        Net:     "udp",
-        Handler: mux,
-    }
+	server := &dns.Server{
+		Addr:    cfg.Listen,
+		Net:     "udp",
+		Handler: mux,
+	}
 
-    log.Printf("DNS server listening on %s", cfg.Listen)
-    return server.ListenAndServe()
+	log.Printf("DNS server listening on %s", cfg.Listen)
+	return server.ListenAndServe()
 }
 
-
-//main function
+// main function
 func main() {
-    //create map
-    db = make(map[uint32]DbEntry)
+	//allocate map
+	QueryDb = make(map[uint32]DbEntry)
 
-    configPath := flag.String("conf", "dnsprint.yaml", "path to config file")
-    flag.Parse()
+	//load config file
+	configPath := flag.String("conf", "dnsprint.yaml", "path to config file")
+	flag.Parse()
 
-    err := loadConfig(*configPath)
-    if err != nil {
-        log.Fatalf("failed to load config: %v", err)
-    }
+	err := loadConfig(*configPath)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
 
+	//load geo db
+	loadGeoDb()
 
-    //run the server
-    if err := listenAndServeDNS(); err != nil {
-        log.Fatalf("server error: %v", err)
-    }
+	//run the server
+	if err := listenAndServeDNS(); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
+
+	//close geo db
+	GeoDb.Close()
 }
